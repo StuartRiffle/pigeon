8a2f187012afb1a17facff855cf807bb82735033
Stuart Riffle <stuart@rifflemail.com>
2016-11-15 18:30:14 -0800

    Gaviota probing code by Miguel A. Ballicora

---
+++ src/tb-gaviota.h
@@ -0,0 +1,20 @@
+// tb-gaviota.h - PIGEON CHESS ENGINE (c) 2012-2016 Stuart Riffle
+
+extern "C" 
+{
+    // This is tablebase probing code by Miguel A. Ballicora
+
+    #include "../contrib/gaviota-tb-0.4.5/gtb-probe.h"  
+    #include "../contrib/gaviota-tb-0.4.5/gtb-dec.h"
+    #include "../contrib/gaviota-tb-0.4.5/gtb-att.h"
+    #include "../contrib/gaviota-tb-0.4.5/gtb-types.h"
+};
+
+namespace Pigeon {
+#ifndef PIGEON_TB_GAVIOTA_H__
+#define PIGEON_TB_GAVIOTA_H__
+
+// TODO
+
+#endif // PIGEON_TB_GAVIOTA_H__
+};
